5865f0410ecfb95ccbb149f3deb445dca1f11669
thinkuldeep <kuldeep_reck@yahoo.com>
2026-06-09 01:55:45 +0530

    Updated.

--- sdui-client/composeApp/src/androidMain/kotlin/com/thinkuldeep/sdui/client/PlatformConfig.kt
+++ sdui-client/composeApp/src/androidMain/kotlin/com/thinkuldeep/sdui/client/PlatformConfig.kt
@@ -4,10 +4,10 @@ import android.os.Build
 import java.util.UUID
 
 actual object PlatformConfig {
-    actual val baseUrl: String = "http://localhost:8080"
+    actual val baseUrl: String = "http://10.0.2.2:8080"
 
     // Jaeger OTLP HTTP receiver endpoint (more reliable than /api/traces)
-    actual val jaegerEndpoint: String = "http://localhost:4318/v1/traces"
+    actual val jaegerEndpoint: String = "http://10.0.2.2:4318/v1/traces"
 
     actual val deviceId: String = UUID.randomUUID().toString()
 
